fa2a14c3f700c1cd4515e197189230ae3d9b37d6
Giovanne Dilly <geovanny.dilly@gmail.com>
2026-06-16 10:11:45 -0300

    entrega backend

--- praticas/pomodoro-frontend/src/workers/TimerWorkerManager.ts
+++ praticas/pomodoro-frontend/src/workers/TimerWorkerManager.ts
@@ -1,3 +1,5 @@
+import type { TaskStateModel } from "../models/TaskStateModel";
+
 let instance: TimerWorkerManager | null = null;
 
 export class TimerWorkerManager {
@@ -11,10 +13,11 @@ export class TimerWorkerManager {
     if (!instance) {
       instance = new TimerWorkerManager();
     }
+
     return instance;
   }
 
-  postMessage(message: any) {
+  postMessage(message: TaskStateModel) {
     this.worker.postMessage(message);
   }
 
